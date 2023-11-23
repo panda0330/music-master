d93ded4490bfa7e9bd653ecc67add2c437be2514
panda0330 <superdev330@gmail.com>
2023-11-24 06:03:23 +0900

    chat change

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -48,6 +48,19 @@ export const ChatComp = () => {
         </div>
         <div className="chat-message">
           <div className="flex items-end justify-end">
+            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+              <div>
+                <span className="px-4 py-2 rounded-full inline-block  bg-[#FF0093] text-white ">
+                  Hey, How you have been?
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-message">
+          <div className="flex items-end">
+            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+              <div>
 
           </div>
         </div>
