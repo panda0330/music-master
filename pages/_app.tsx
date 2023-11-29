2a09ef4153f59400103597599b0e995d7d4e63e6
panda0330 <superdev330@gmail.com>
2023-11-29 13:29:19 +0900

    change _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,6 +43,30 @@ const theme = createTheme({
   },
 });
 
+const Header = () => {
+  return (
+    <Head>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+      />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, shrink-to-fit=no, minimal-ui"
+      />
+      <meta property="og:url" content="https://stocks.valyoux.io" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="Valyouxmusic" />
+      <meta
+        property="og:description"
+        content="The World's First Music Artist Stock Market Powered by Blockchain Technology"
+      />
+      <meta
+        property="og:image"
+        content="https://stocks.valyoux.io/public/assets/images/valyou_x_black_logo.svg"
+      />
 
 
 export default wrapper.withRedux(MyApp);
