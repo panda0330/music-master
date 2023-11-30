fce9a5cd10b727153b945971b6e0819de7d63b9e
panda0330 <superdev330@gmail.com>
2023-12-01 08:16:06 +0900

    change investment

--- pages/investment.tsx
+++ pages/investment.tsx
@@ -15,6 +15,24 @@ const investment = () => {
           <div className="p-1 text-white bg-black rounded-full">
             <Link href="/wallet">
               <KeyboardBackspaceIcon />
+            </Link>
+          </div>
+          <b className="text-2xl"></b>
+        </div>
+        {/* <div className="flex items-center justify-between w-[100%]">
+          <div className="bg-black rounded-full m-1">
+            <IconButton style={{ color: "white" }}>
+              <CloseIcon></CloseIcon>
+            </IconButton>
+          </div>
+        </div> */}
+        {/* <Card className="border">
+          <CardContent sx={{ paddingBottom: "16px !important" }}>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <Avatar
+                  sx={{ width: 40, height: 40 }}
+                  className="border-2 border-black"
 
         </div>
       </div>
