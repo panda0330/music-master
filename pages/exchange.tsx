260c4f508dd3ef7db0342ab799398f67c399b18a
panda0330 <superdev330@gmail.com>
2023-11-30 10:11:04 +0900

    change exchange

--- pages/exchange.tsx
+++ pages/exchange.tsx
@@ -19,34 +19,7 @@ const Exchange = () => {
             <Link href="/wallet">
               <KeyboardBackspaceIcon />
             </Link>
-          </div>
-          <b className="text-2xl">EXCHANGE VXD</b>
-        </div>
-        <div className="w-full md:w-[376px] h-56 p-4 rounded-lg font-bold bg-black text-white shadow my-1">
-          <div className="flex justify-between items-center">
-            <Image
-              src="/assets/valyoux_black_logo.svg"
-              width={50}
-              height={50}
-              alt="valyoux logo"
-            />
-            <div className="flex flex-col items-end">
-              <div>Valyou X Dollars - Platform Currency</div>
-            </div>
-          </div>
-          <div className="py-10 flex justify-center text-2xl">
-            <span>VXD : $ 16,000</span>
-          </div>
-        </div>
-        <div className="background-shadow p-2 my-1">
-          <div className="flex justify-between items-center p-1 mb-2">
-            <p className="font-[500]">Select Exchange Method</p>
-          </div>
-          <RadioSelect value={radioType} onChange={changeRadioType} />
-          <br />
-          {radioType == "crypto" ? <Crypto /> : <Fiat />}
-        </div>
-      </div>
+
     </>
   );
 };
